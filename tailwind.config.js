3cddf0773cdc29b120e59f7b5012394bf02d231d
Josef Bauer <josef.bauer.1st@gmail.com>
2024-12-05 08:44:35 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+import forms from "@tailwindcss/forms";
+import daisyui from "daisyui";
+
+/** @type {import("tailwindcss").Config} */
+export default {
+  plugins: [forms, daisyui],
+
+  content: [
+    "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+    "./storage/framework/views/*.php",
+    "./resources/views/**/*.blade.php",
+  ],
+
+  daisyui: {
+    themes: ["business"],
+  },
+};
