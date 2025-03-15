b8a55515096206ee99d38866c8807b19b5fe3c51
Josef Bauer <josef.bauer.1st@gmail.com>
2025-03-15 21:17:46 +0100

    WIP: navbar

--- resources/views/components/nav/nav-bar.blade.php
+++ resources/views/components/nav/nav-bar.blade.php
@@ -1,4 +1,4 @@
-<nav id="header" class="border-opacity-25 h-12 border-b border-b-white" x-data="{ showMenu: false }">
+<nav id="header" class="border-base-content/25 h-12 border-b" x-data="{ showMenu: false }">
     <div class="px-4 sm:px-6 lg:px-8">
         <div class="flex h-12 items-center justify-between">
             <div class="flex gap-4">
@@ -37,14 +37,14 @@
     <!-- Mobile menu -->
     <!-- ----------- -->
     <div
-        class="bg-base-300 absolute top-10 right-2 z-50 rounded-md border-1 select-none md:hidden"
-        x-transition.duration.250ms
+        class="bg-base-300 absolute top-10 right-2 z-50 rounded-md select-none md:hidden"
+        x-transition.duration.100ms
         x-show="showMenu"
         @click.outside="showMenu = false"
     >
         <div class="flex flex-col gap-5 p-6 sm:px-3">
             @if (Auth::user()?->isDealer())
-                <x-nav.nav-menu-entry text="Basic vs Pro" href="/login" icon="rocket" />
+                <x-nav.nav-menu-entry :text="__('Basic vs Pro')" href="/login" icon="rocket" />
                 <x-nav.nav-menu-entry text="Basic vs Pro" href="/login" icon="rocket" />
                 <x-nav.nav-menu-entry text="Basic vs Pro" href="/login" icon="rocket" />
             @elseif (Auth::user()?->isUser())
@@ -53,20 +53,22 @@
                 <x-nav.nav-menu-entry text="Basic vs Pro" href="/login" icon="rocket" />
             @endif
 
-            <x-nav.nav-menu-entry text="Login" href="/login" icon="login" />
-            <x-nav.nav-menu-entry text="Signup" href="/signup" icon="signup" />
+            <x-nav.nav-menu-entry :text="__('Login')" href="/login" icon="login" />
+            <x-nav.nav-menu-entry :text="__('Signup')" href="/signup" icon="signup" />
             <div class="flex gap-4">
-                { t("language", lang) }:
+                {{ __("Language") }}:
                 <div class="flex gap-2">
                     <a class="link w-10" href="/language-set/de"><img src="/icons/flag-de.svg" alt="Flag Germany" /></a>
                     <a class="link w-10" href="/language-set/en"><img src="/icons/flag-uk.svg" alt="Flag UK" /></a>
                 </div>
             </div>
-            <button onclick="LocationService.toggleLocationServiceSimulation()">Toggle Walk Simulator</button>
+            <button class="btn btn-primary" onclick="LocationService.toggleLocationServiceSimulation()">
+                Toggle Walk Simulator
+            </button>
             <div class="flex justify-around gap-8 pt-8 text-xs">
-                <a href="/imprint">{ t("imprint", lang) }</a>
-                <a href="/terms">{ t("terms", lang) }</a>
-                <a href="/privacy">{ t("privacy", lang) }</a>
+                <a href="/imprint">{{ __("Imprint") }}</a>
+                <a href="/terms">{{ __("Terms") }}</a>
+                <a href="/privacy">{{ __("Privacy") }}</a>
             </div>
         </div>
     </div>
